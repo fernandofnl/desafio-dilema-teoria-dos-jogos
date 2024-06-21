10d421632779aa0a0bdd33b1f42a96b32b76dff4
fernandofnl <fernandofnl@gmail.com>
2024-06-21 20:49:25 -0300

    inclusao do modo de jogo 2 e 3

--- dilema_prisioneiro.py
+++ dilema_prisioneiro.py
@@ -16,34 +16,36 @@ Escolha uma opção de jogo:
 programa = True
 validacao = True
 repetir_partida = True
+
+#contadores modo de jogo
+contador_jogo_simples = 0
+contador_olho_por_olho = 0
+contador_olho_por_dois_olhos = 0
+contador_provador_ingenuo = 0
+contador_retalhador_permanente = 0
+
+# contadores dos resultados das partidas
 contador_trair_trair = 0
 contador_cooperar_trair = 0
 contador_trair_cooperar = 0
 contador_cooperar_cooperar = 0
-numero_partidas = 1
 
-resultado = f"""
-==== Resultados ====
+numero_partidas = 0
+escolheu_trair = 0
+escolheu_cooperar = 0
 
-Você jogou {numero_partidas} partidas.
-
-Quantidade de vezes:
-- Trair x Trair: {(contador_trair_trair * 100) / numero_partidas}%
-- Trair x Cooperar: {(contador_trair_cooperar * 100) / numero_partidas}%
-- Cooperar x Trair: {(contador_trair_trair * 100) / numero_partidas}%
-- Cooperar x Cooperar: {(contador_cooperar_cooperar * 100) / numero_partidas}%
-
-"""
 
 while programa:
 
     print(menu)
+    
     opcao_de_jogo = int(input("Escolha uma opção para jogar: "))
 
     while validacao:
-        
+    
         if  opcao_de_jogo > 5 or opcao_de_jogo < 0:
             print("Opção inválida!")
+            opcao_de_jogo = int(input("Escolha uma opção para jogar: "))
 
         else:
             validacao = False
@@ -53,10 +55,12 @@ while programa:
         while True:
 
             print(f"\n==== Jogo Simples ====")
-            numero_partidas += 1
+            
+            contador_jogo_simples += 1
             escolha_computador = random.randint(1, 2)
 
             decisao_jogador = int(input("Qual sua decisão? 1. Trair ou 2. Cooperar: "))
+            numero_partidas += 1
 
             if decisao_jogador == 1 and escolha_computador == 1:
 
@@ -78,12 +82,118 @@ while programa:
                 print(f"\nAmbos os jogadores escolheram cooperar.")
                 contador_cooperar_cooperar += 1
 
-            jogar_novamente = int(input("Você deseja jogar novamente? 1. Sim ou 2. Não: "))
+            jogar_novamente = int(input(f"\nVocê deseja jogar novamente? 1. Sim ou 2. Não: "))
 
             if jogar_novamente == 2:
                 break
 
+    elif opcao_de_jogo == 2:
 
+        escolha_computador = random.randint(1, 2)
+        
+        while True:
+
+            print(f"\n==== Olho por olho ====")
+            numero_partidas += 1
+            contador_olho_por_dois_olhos += 1
+            
+            decisao_jogador = int(input("Qual sua decisão? 1. Trair ou 2. Cooperar: "))
+
+            if decisao_jogador == 1 and escolha_computador == 1:
+
+                print(f"\nAmbos os jogadores escolheram trair.")
+                escolha_computador = 1
+                contador_trair_trair += 1
+
+            elif decisao_jogador == 1 and escolha_computador == 2:
+
+                print(f"\nVocê escolheu trair e o seu parceiro escolheu cooperar.")
+                escolha_computador = 1
+                contador_trair_cooperar += 1
+
+            elif decisao_jogador == 2 and escolha_computador == 1:
+
+                print(f"\nVocê escolheu cooperar e seu parceiro escolheu trair.")
+                escolha_computador = 2
+                contador_cooperar_trair += 1
+
+            elif decisao_jogador == 2 and escolha_computador == 2:
+
+                print(f"\nAmbos os jogadores escolheram cooperar.")
+                escolha_computador = 2
+                contador_cooperar_cooperar += 1
+
+            jogar_novamente = int(input(f"\nVocê deseja jogar novamente? 1. Sim ou 2. Não: "))
+
+            if jogar_novamente == 2:
+                break
+
+    elif opcao_de_jogo == 3:
+
+        contador_de_traicao = 0
+
+        while True:
+
+            print(f"\n==== Olho por dois olhos ====")
+            numero_partidas += 1
+            contador_olho_por_dois_olhos += 1
+            escolha_computador = 2
+
+            if contador_de_traicao >= 2:
+                escolha_computador = 1
+
+            decisao_jogador = int(input("Qual sua decisão? 1. Trair ou 2. Cooperar: "))
+
+            if decisao_jogador == 1 and escolha_computador == 1:
+
+                print(f"\nAmbos os jogadores escolheram trair.")
+                escolha_computador = 1
+                contador_trair_trair += 1
+                contador_de_traicao += 1
+
+            elif decisao_jogador == 1 and escolha_computador == 2:
+
+                print(f"\nVocê escolheu trair e o seu parceiro escolheu cooperar.")
+                escolha_computador = 1
+                contador_trair_cooperar += 1
+                contador_de_traicao += 1
+
+            elif decisao_jogador == 2 and escolha_computador == 1:
+
+                print(f"\nVocê escolheu cooperar e seu parceiro escolheu trair.")
+                escolha_computador = 2
+                contador_cooperar_trair += 1
+                contador_de_traicao = 0
+
+            elif decisao_jogador == 2 and escolha_computador == 2:
+
+                print(f"\nAmbos os jogadores escolheram cooperar.")
+                escolha_computador = 2
+                contador_cooperar_cooperar += 1
+                contador_de_traicao = 0
+
+            jogar_novamente = int(input(f"\nVocê deseja jogar novamente? 1. Sim ou 2. Não: "))
+
+            if jogar_novamente == 2:
+                break
+    
     elif opcao_de_jogo == 0:
 
         programa = False
+        relatorio = f"""
+==== Relatorio ====
+
+Você jogou {numero_partidas} partidas.
+
+Resultados das partidas:
+- Trair x Trair: {((contador_trair_trair * 100) / numero_partidas):.1f}%
+- Trair x Cooperar: {((contador_trair_cooperar * 100) / numero_partidas):.1f}%
+- Cooperar x Trair: {((contador_trair_trair * 100) / numero_partidas):.1f}%
+- Cooperar x Cooperar: {((contador_cooperar_cooperar * 100) / numero_partidas):.1f}%
+
+Você escolheu:
+- {escolheu_trair} vezes Trair        -   {(escolheu_trair * 100 / numero_partidas):.1f} das partidas
+- {escolheu_cooperar} vezes Cooperar  -   {(escolheu_cooperar * 100 / numero_partidas):.1f} das partidas
+"""
+        
+print(relatorio)
